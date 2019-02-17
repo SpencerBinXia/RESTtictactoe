52b1a82a5f77cba47287765e9b0e654e9d7bc482
Spencer X <spencerbinxia@gmail.com>
2019-02-17 02:27:26 -0500

    Commit: Name/date POST request working

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/ttt', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/ttt', function(req, res, next)  {
+  res.render('index', { title: 'Tic Tac Toe'});
+});
+
+router.post('/ttt', function(req, res, next) {
+    var name = req.body.name;
+    var today = new Date().toLocaleString();
+    console.log(req.body.name);
+    console.log("anything");
+    console.log(today);
+    res.render('index', { title: 'Tic Tac Toe', name: name, date: today});
 });
 
 module.exports = router;
